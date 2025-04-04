47cdfee430d20f6f2114e4c05ac3f55569ac2e75
thameur.aissaoui <thameur.aissaoui@gmail.com>
2025-04-04 06:18:58 -0400

    first commit

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,18 +1,14 @@
-import axios from "axios";
-
-const BASE_URL =
-  process.env.NODE_ENV === "development"
-    ? "/api" // use proxy in development
-    : "https://api-dev.pokeadeal.com/api"; // full URL in production
+const PROXY = "https://corsproxy.io/?"; // or another CORS proxy
+const BASE_URL = "https://api-dev.pokeadeal.com/api";
 
 export const registerProfessional = async (data) => {
-  return axios.post(`${BASE_URL}/Authenticate/register-professional`, data);
+  return axios.post(`${PROXY}${BASE_URL}/Authenticate/register-professional`, data);
 };
 
-export const loginConsumer = async (data) => {
-  return axios.post(`${BASE_URL}/Authenticate/login-professional`, data);
+export const loginProfessional = async (data) => {
+  return axios.post(`${PROXY}${BASE_URL}/Authenticate/login-professional`, data);
 };
 
 export const forgotPassword = async (email) => {
-  return axios.post(`${BASE_URL}/Authenticate/forgot-password-professional`, { email });
+  return axios.post(`${PROXY}${BASE_URL}/Authenticate/forgot-password-professional`, { email });
 };
